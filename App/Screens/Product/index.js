1a1bd6cfa76cea319c71269be43e2265d4157d4a
alex.fan <alex.fan@okchem.com>
2018-05-10 10:22:03 +0800

    product page update

--- App/Screens/Product/index.js
+++ App/Screens/Product/index.js
@@ -1,19 +1,41 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import {
+  Container,
+  Content,
+  Header,
+  Footer,
+  Left,
+  Body,
+} from 'native-base'
+import { Text } from 'react-native'
 
-class MeScreen extends React.Component {
+class ProductScreen extends React.Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'red',
-      }}>
-        <Text>Product Details!</Text>
-      </View>
+      <Container>
+        <Header style={{backgroundColor:'green'}}>
+          <Left>
+            <Text>
+              Back
+            </Text>
+          </Left>
+          <Body>
+            Product
+          </Body>
+        </Header>
+        <Content style={{backgroundColor:'blue'}}>
+          <Text>
+            Content
+          </Text>
+        </Content>
+        <Footer style={{backgroundColor:'red'}}>
+          <Text>
+            Footer Text
+          </Text>
+        </Footer>
+      </Container>
     );
   }
 }
 
-export default MeScreen
+export default ProductScreen
